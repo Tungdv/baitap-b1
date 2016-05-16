818deaf37b5a6d1d6aa9c3f3f59b22906e7fe1a2
Tungdv <tdesign91@gmail.com>
2016-05-16 23:24:08 +0700

    12313
    123213

---
+++ baitap-include/header.php
@@ -0,0 +1,12 @@
+<link href="css/stylesheet.css" type="text/css" rel="stylesheet" />
+<div id="header">
+	<h1 class="title">HOCTUDAU.COM</h1>
+	<div class="menu">
+		<ul>
+			<li><a href="/baitap-b1/baitap-include/index.php">HOME</a></li>
+			<li><a href="/baitap-b1/baitap-include/about.php">ABOUT</a></li>
+			<li><a href="/baitap-b1/baitap-include/testimonial.php">TESTIMONIAL</a></li>
+			<li><a href="/baitap-b1/baitap-include/contact.php">CONTACT</a></li>
+		</ul>
+	</div>
+</div>
---
+++ timhieu_get.php
@@ -0,0 +1,2 @@
+<h1 style="color: <?php echo $_GET['a']; ?>">Hoctudau.com</h1>
+<p style="color: <?php echo $_GET['a']; ?>">Trung tâm đào tạo lập trình web</p>
